ff013b4a9c664307e8ac8c31f7eae5fb38688fdf
Arya Baskara <aryabasc@gmail.com>
2026-04-07 09:13:56 +0700

    feat: handle empty argument and join arguments to string

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 const version = '0.0.1';
 
 void main(List<String> arguments) {
@@ -14,7 +16,20 @@ void main(List<String> arguments) {
 }
 
 void searchWikipedia(List<String>? arguments) {
-  print('searchWikipedia received arguments: $arguments');
+  final String articleTitle;
+
+  // If no article title is provided as an argument, prompt the user to enter one.
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+
+    // Await user input from the console.
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    // Join the arguments to form the full article title.
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Current article: $articleTitle');
 }
 
 void printUsage() {
