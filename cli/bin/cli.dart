6e610b5d9afab5f1f767fff62affad86434d54e9
Arya Baskara <aryabasc@gmail.com>
2026-04-09 10:34:34 +0700

    refactor: revert main function to synchronous execution and remove unused imports

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,10 +1,8 @@
-import 'dart:io';
-import 'package:http/http.dart' as http;
 import 'package:command_runner/command_runner.dart';
 
 const version = '0.0.1';
 
-void main(List<String> arguments) async { // main is now async and awaits the runner.
-  var runner = CommandRunner(); // Create an instance of the command runner.
-  await runner.run(arguments); // Run the command runner, awaiting its Future<void>.
+void main(List<String> arguments) {
+  var runner = CommandRunner()..addCommand(HelpCommand());
+  runner.run(arguments);
 }
